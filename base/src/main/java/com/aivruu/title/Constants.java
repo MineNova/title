4a1c3d66fe87b54ce016c6a5c6a34b8552fbd210
InitSync <benjaalejandro206@gmail.com>
2023-08-30 18:19:39 -0400

    chore: remove old library code

--- base/src/main/java/com/aivruu/title/Constants.java
+++
@@ -1,7 +0,0 @@
-package com.aivruu.title;
-
-import org.bukkit.Bukkit;
-
-public class Constants {
-	public static final int SERVER_VERSION = Integer.parseInt(Bukkit.getBukkitVersion().split("-")[0].split("\\.")[1]);
-}
